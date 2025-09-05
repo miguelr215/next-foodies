5e3f52504f48f161705dafccb75f3ac64a7f2bfc
miguelr215 <drexelramos@gmail.com>
2025-09-05 06:32:48 -0400

    meals grid data

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 import styles from './page.module.css';
 import Link from 'next/link';
 import MealsGrid from '../components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
-const MealsPage = () => {
+const MealsPage = async () => {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={styles.header}>
@@ -19,7 +22,7 @@ const MealsPage = () => {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   )
